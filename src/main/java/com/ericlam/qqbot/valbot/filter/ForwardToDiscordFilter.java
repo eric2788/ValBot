069a3b73f7662cc8e4121a21d4f2b66b8934698b
eric2788 <tsukiko852@gmail.com>
2021-09-14 16:57:44 +0800

    fixed multiple images in one message always return the first image url

--- src/main/java/com/ericlam/qqbot/valbot/filter/ForwardToDiscordFilter.java
+++ src/main/java/com/ericlam/qqbot/valbot/filter/ForwardToDiscordFilter.java
@@ -1,13 +1,14 @@
 package com.ericlam.qqbot.valbot.filter;
 
-import com.ericlam.qqbot.valbot.RequestException;
 import com.ericlam.qqbot.valbot.configuration.properties.DiscordConfig;
 import com.ericlam.qqbot.valbot.dto.res.ForwardedContent;
 import com.ericlam.qqbot.valbot.service.QQBotService;
+import com.mikuac.shiro.common.utils.RegexUtils;
 import com.mikuac.shiro.common.utils.ShiroUtils;
 import com.mikuac.shiro.core.Bot;
 import com.mikuac.shiro.core.BotPlugin;
 import com.mikuac.shiro.dto.event.message.PrivateMessageEvent;
+import com.mikuac.shiro.enums.ShiroUtilsEnum;
 import discord4j.common.util.Snowflake;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.object.entity.channel.GuildMessageChannel;
@@ -16,12 +17,10 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Scheduler;
-import reactor.scheduler.forkjoin.ForkJoinPoolScheduler;
+import reactor.core.scheduler.Schedulers;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ForkJoinPool;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -52,18 +51,49 @@ public class ForwardToDiscordFilter extends BotPlugin {
         List<ForwardedContent.ForwardMessage> list;
         try {
             list = botService.getForwardMsg(bot, matcher.group(1)).getData().getMessages();
-        }catch (Exception e){
-            bot.sendPrivateMsg(event.getUserId(), "消息转发失败:"+e.getMessage(), true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            bot.sendPrivateMsg(event.getUserId(), "消息转发失败:" + e.getMessage(), true);
             return MESSAGE_BLOCK;
         }
-        Flux.just(list.toArray(ForwardedContent.ForwardMessage[]::new))
-                .subscribeOn(ForkJoinPoolScheduler.create("forward-to-discord")) // async
-                .flatMap(msg -> Flux.just(ShiroUtils.getMsgImgUrlList(msg.getContent()).toArray(String[]::new)))
+        this.recursiveForward(bot, Flux.fromIterable(list))
+                .subscribeOn(Schedulers.boundedElastic())
+                .flatMap(msg -> Flux.just(getMsgImgUrlList(msg.getContent()).toArray(String[]::new)))
+                .distinct()
                 .concatMap(s -> client.getChannelById(Snowflake.of(discordConfig.getNsfwChannel()))
                         .ofType(GuildMessageChannel.class).flatMap(ch -> ch.createMessage(s)))
                 .doOnComplete(() -> bot.sendPrivateMsg(event.getUserId(), "消息转发成功", true))
-                .doOnError(ex -> bot.sendPrivateMsg(event.getUserId(), "消息转发失败:"+ex.getMessage(), true))
+                .doOnError(ex -> bot.sendPrivateMsg(event.getUserId(), "消息转发失败:" + ex.getMessage(), true))
                 .subscribe();
         return MESSAGE_BLOCK;
+    }
+
+
+    private Flux<ForwardedContent.ForwardMessage> recursiveForward(Bot bot, Flux<ForwardedContent.ForwardMessage> messages) {
+        return messages
+                .publishOn(Schedulers.boundedElastic())
+                .expandDeep(msg -> {
+                    Matcher matcher = FORWARD_PATTERN.matcher(msg.getContent());
+                    try {
+                        if (matcher.find()) {
+                            List<ForwardedContent.ForwardMessage> list = botService.getForwardMsg(bot, matcher.group(1)).getData().getMessages();
+                            return this.recursiveForward(bot, Flux.fromIterable(list));
+                        }
+                    }catch (Exception e){
+                        logger.warn("Error while parsing nested content: "+e.getMessage(), e);
+                    }
+                    return Flux.empty();
+                });
+
+    }
+
+    public static List<String> getMsgImgUrlList(String msg) {
+        List<String> imgUrlList = new ArrayList<>();
+        for (String i : msg.split(ShiroUtilsEnum.CQ_CODE_SPLIT.getValue())) {
+            if (i.startsWith("image")) {
+                imgUrlList.add(RegexUtils.regex(ShiroUtilsEnum.GET_URL_REGEX.getValue(), i));
+            }
+        }
+        return imgUrlList;
     }
 }
